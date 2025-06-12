de5bad5266bb283f268ad53750decd343278e3ae
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-06-12 12:53:39 -0300

    feat: add Redis caching to instrument search functionality and configure Redis service in docker-compose

--- src/application/use_cases.py
+++ src/application/use_cases.py
@@ -2,6 +2,7 @@ import pandas as pd
 import hashlib
 from typing import List, IO, Optional
 from src.infrastructure.database import repositories
+from src.infrastructure.cache import redis_cache
 
 def process_file_upload (file: IO, filename: str):
     """
@@ -38,7 +39,23 @@ def process_file_upload (file: IO, filename: str):
     return {"filename": filename, "message": "Arquivo processado com sucesso"}
 
 def find_instruments(TckrSymb: Optional[str]=None, RptDt: Optional[str]=None, page: int = 1, page_size: int = 20):
-    """Caso de uso para buscar instrumentos."""
+    """Caso de uso para buscar instrumentos. Agora com lógica de cache."""
+     # 1. Cria uma chave de cache única para esta busca específica.
+    cache_key = f"find_instruments:{TckrSymb}:{RptDt}:{page}:{page_size}"
+
+    # 2. Tenta buscar os dados do cache primeiro.
+    cached_result = redis_cache.get_from_cache(cache_key)
+    if cached_result is not None:
+        return cached_result
+    
+    # 3. Se não houver cache (cache miss), busca no banco de dados.
+    db_result = repositories.search_instruments(TckrSymb, RptDt, page, page_size)
+
+    # 4. Salva o resultado do banco no cache para futuras requisições.
+    if db_result:
+        redis_cache.set_in_cache(cache_key, db_result)
+    
+    return db_result
     return repositories.search_instruments(TckrSymb, RptDt, page, page_size)
 def list_upload_history(filename: Optional[str]=None, upload_date: Optional[str]=None):
     """Caso de uso para listar o histórico de upload de arquivos."""
